974ffdc9d4a0989bd20630656c55857802714a70
morkeseth <pamoerk@gmail.com>
2016-05-12 15:03:29 +0200

    ta en titt, jørn

--- Innlevering3/Controllers/Innlevering3Controller.cs
+++ Innlevering3/Controllers/Innlevering3Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Innlevering3.Models;
 
 namespace Innlevering3.Controllers
 {
@@ -12,6 +13,70 @@ namespace Innlevering3.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult VisAlleHenvendelser()
+        {
+            try
+            {
+                using (Innlevering3DatabaseEntities innlevering3DBTilkobling = new Innlevering3DatabaseEntities())
+                {
+                    var henvendelserListe = (from henvendelser in innlevering3DBTilkobling.Henvendelser
+                                     select henvendelser).ToList();
+
+                    return View(henvendelserListe);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult VisAlleAnsatte()
+        {
+            try
+            {
+                using (Innlevering3DatabaseEntities innlevering3DBTilkobling = new Innlevering3DatabaseEntities())
+                {
+                    var ansatteListe = (from ansatte in innlevering3DBTilkobling.Ansatte
+                                        select ansatte).ToList();
+
+                    return View(ansatteListe);
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult RegistrerNyAnsatt()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegistrerNyAnsatt(Ansatte nyAnsatt)
+        {
+            if (ModelState.IsValid)
+            {
+                using (Innlevering3DatabaseEntities innlevering3DBTilkobling = new Innlevering3DatabaseEntities())
+                {
+                    innlevering3DBTilkobling.Ansatte.Add(nyAnsatt);
+                    innlevering3DBTilkobling.SaveChanges();
+                }
+
+                Session["SisteAnsatt"] = nyAnsatt.Fornavn;
+
+                return RedirectToAction("VisAlleAnsatte");
+            }
+            else
+            {
+                return View();
+            }
+
+
         }
     }
 }
