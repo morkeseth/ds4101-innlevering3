2cd8e6f121a253a4e486db7c00f8f0dff19f8e56
morkeseth <pamoerk@gmail.com>
2016-05-12 12:24:44 +0200

    initial

---
+++ Innlevering3/Controllers/Innlevering3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Innlevering3.Controllers
+{
+    public class Innlevering3Controller : Controller
+    {
+        // GET: Innlevering3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Innlevering3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Innlevering3.MvcApplication" Language="C#" %>
